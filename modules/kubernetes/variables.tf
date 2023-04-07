d0152f64baf38b5be61efbb58c5eb0fd5bcc2abf
Nursultan Begmatov <nbegmatov@proofpoint.com>
2023-04-07 17:11:58 -0600

    reverting the changes for now

--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -1,25 +1,25 @@
-locals {
-  enabled = contains([""], var.namespace) ? 1 : 0
-  eks_cluster_config = var.eks_cluster_config[var.namespace]
-}
-
-variable "namespace" {}
-
-variable "region" {
-  description = "Region in which the bastion host will be launched"
-}
-
-variable "vpc_id" {}
-
-variable "eks_cluster_config" {
-  type = map
-  default = {
-    lab = {
-      eks_version = "1.21"
-    }
-    prod = {
-      eks_version = "1.21"
-    }
-  }
-}
-
+//locals {
+//  enabled = contains([""], var.namespace) ? 1 : 0
+//  eks_cluster_config = var.eks_cluster_config[var.namespace]
+//}
+//
+//variable "namespace" {}
+//
+//variable "region" {
+//  description = "Region in which the bastion host will be launched"
+//}
+//
+//variable "vpc_id" {}
+//
+//variable "eks_cluster_config" {
+//  type = map
+//  default = {
+//    lab = {
+//      eks_version = "1.21"
+//    }
+//    prod = {
+//      eks_version = "1.21"
+//    }
+//  }
+//}
+//
--- modules/networking/vpc.tf
+++ modules/networking/vpc.tf
@@ -99,7 +99,7 @@ resource "aws_route_table" "public" {
 # Route for Internet Gateway
 resource "aws_route" "public_internet_gateway" {
   count = local.enabled
-  route_table_id         = aws_route_table.public[0].id
+  route_table_did         = aws_route_table.public[0].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.ig[0].id
 }
